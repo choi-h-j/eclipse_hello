9397b6926c7a876ae189db68cebae72653100874
choi <choisej1@naver.com>
2019-11-06 09:38:12 +0900

    added line

--- Information/src/information.java
+++ Information/src/information.java
@@ -4,6 +4,7 @@ public class information {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Computer SW");
+		System.out.println("Wonkwang Univ");
 	}
 
 }
