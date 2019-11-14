9dba5f9c4081e7b972ce30038c107b57a9349fd3
rhqudco <rhqudco1204@naver.com>
2019-11-14 15:09:57 +0900

    PushPrint.java modified on Local repository

--- Information/src/PushPrint.java
+++ Information/src/PushPrint.java
@@ -4,6 +4,6 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Add Java Class File Push");
-			System.out.println("World with Choi"); 
+			System.out.println("World with Choi");
 	}
 }
